476f16ca63ead2ef53a8847c932ebbe092d6cbb6
zooozoo <shz0309@gmail.com>
2017-10-22 00:13:45 +0900

    19page 에러메시지 똑같이 안남

--- superlitsts/ft.py
+++ superlitsts/ft.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest():
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish the test!")
+
+
+
+# browser = webdriver.Chrome()
+# browser.get('http://localhost:8000')
+#
+# assert 'To-Do' in browser.title, "Browser title was " + browser.title
+#
+# browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
