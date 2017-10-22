d899d50491547e340f88d0d76cbca61fd4cc2af4
zooozoo <shz0309@gmail.com>
2017-10-22 23:29:29 +0900

    아이템용 모델과 마이그레이션 연계

--- superlitsts/ft.py
+++ superlitsts/ft.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -12,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -20,30 +24,16 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            '작업 아이템 입력'
-        )
-
-        # inputbox.send_keys('공작깃털 사기')
-        #
-        # inputbox.send_keys(Keys.ENTER)
-        #
-        # table = self.browser.find_element_by_id('id_list_table')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertIn('1: 공잣깃털 사기', [row.text for row in rows])
+        inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('공작깃털을 이용해서 그물 만들기')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         self.fail("Finish the test!")
 
--- superlitsts/lists/tests.py
+++ superlitsts/lists/tests.py
@@ -1,10 +1,10 @@
 import re
-
+from .models import Item
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.views import home_page
+from .views import home_page
 
 
 class HomePageTest(TestCase):
@@ -42,3 +42,22 @@ class HomePageTest(TestCase):
             re.sub(self.pattern_input_csrf, '', expected_html)
         )
 
+
+class ItemModelTest(TestCase):
+
+    def text_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
