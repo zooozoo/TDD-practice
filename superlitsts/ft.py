e16684620f9e290cb9f732119f49fb97e0aecdd3
zooozoo <shz0309@gmail.com>
2017-10-22 20:48:00 +0900

    53page 템플릿을 이용한 메인 페이지 html 생성

--- superlitsts/ft.py
+++ superlitsts/ft.py
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            '직업 아이템 입력'
+            '작업 아이템 입력'
         )
 
         inputbox.send_keys('공작깃털 사기')
@@ -32,6 +32,7 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1; 공작기털 사기' for row in rows),
+            "신규 작업이 테이블에 표시되지 않는다."
         )
         self.fail("Finish the test!")
 
