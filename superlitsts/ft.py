fdaadc65f09f208830c2ffbe7b3ea44fe26c2361
zooozoo <shz0309@gmail.com>
2017-10-22 16:08:58 +0900

    44p 기능 테스트가 작업 아이템을 추가할 수 있는지 확인"

--- superlitsts/ft.py
+++ superlitsts/ft.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVisitorTest():
+class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -14,15 +15,26 @@ class NewVisitorTest():
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '직업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1; 공작기털 사기' for row in rows),
+        )
         self.fail("Finish the test!")
 
-
-# browser = webdriver.Chrome()
-# browser.get('http://localhost:8000')
-#
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title
-#
-# browser.quit()
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- superlitsts/lists/tests.py
+++ superlitsts/lists/tests.py
@@ -6,7 +6,6 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
